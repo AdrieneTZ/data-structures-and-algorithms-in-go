e56231507e0db0074e915212c17efe0a9b23289d
AdrieneTZ <tznali17@gmail.com>
2024-11-06 15:00:04 +0800

    linked lists constructor

--- linked-lists/linked-lists.go
+++ linked-lists/linked-lists.go
@@ -1,1 +1,17 @@
-package linked_lists
+package main
+
+type Node struct {
+	Value interface{}
+	Next  *Node // 指向下一個node struct的記憶體位置
+}
+
+type LinkedList struct {
+	head *Node // 指向第一個node struct的記憶體位置
+	tail *Node // 指向最後一個node struct的記憶體位置
+}
+
+// 建立新的記憶體空間，用來存放 LinkedList struct
+// 同時初始化所有欄位為零值(head = nil, tail = nil)
+func New() *LinkedList {
+	return new(LinkedList)
+}
