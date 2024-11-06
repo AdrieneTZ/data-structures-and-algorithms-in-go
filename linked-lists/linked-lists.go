c125508818db0f92b47069971424c927c6766b40
AdrieneTZ <tznali17@gmail.com>
2024-11-06 15:10:17 +0800

    linked list method: append

--- linked-lists/linked-lists.go
+++ linked-lists/linked-lists.go
@@ -15,3 +15,36 @@ type LinkedList struct {
 func New() *LinkedList {
 	return new(LinkedList)
 }
+
+func NewNode(value interface{}) *Node {
+	n := new(Node)
+	n.Value = value
+	return n
+}
+
+// 實作methods
+/*
+Public methods
+*/
+
+// Append: 將節點插入結尾
+func (ll *LinkedList) Append(n *Node) {
+	// 檢查linked list是否是空的
+	if ll.checkIsEmpty() {
+		ll.head = n
+		ll.tail = n
+		return
+	}
+
+	ll.tail.Next = n // 原本是 nil，現在指向新節點的記憶體位置
+	ll.tail = n      // 把linked list的最後一個節點改成新節點
+}
+
+/*
+Private methods
+*/
+
+// checkIsEmpty: 檢查linked list是否是空的
+func (ll *LinkedList) checkIsEmpty() bool {
+	return ll.head == nil
+}
